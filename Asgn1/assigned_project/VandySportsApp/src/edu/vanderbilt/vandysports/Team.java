9f6d89be30446537e29bb6f129c472c778bb9dd0
willpascucci <willpascucci@10.67.235.16>
2014-09-21 16:38:02 -0500

    Added Team class

---
+++ Asgn1/assigned_project/VandySportsApp/src/edu/vanderbilt/vandysports/Team.java
@@ -0,0 +1,14 @@
+package edu.vanderbilt.vandysports;
+
+public class Team
+{
+	public final String Name;
+	public final String Website_Link;
+	
+	
+	public Team(String name, String link)
+	{
+		Name=name;
+		Website_Link=link;
+	}
+}
