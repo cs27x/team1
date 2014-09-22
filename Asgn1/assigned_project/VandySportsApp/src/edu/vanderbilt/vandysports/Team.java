31686b665f605b0f101034affc6496dcc40a6660
willpascucci <willpascucci@10.67.235.16>
2014-09-21 22:00:35 -0500

    changes to team class

--- Asgn1/assigned_project/VandySportsApp/src/edu/vanderbilt/vandysports/Team.java
+++ Asgn1/assigned_project/VandySportsApp/src/edu/vanderbilt/vandysports/Team.java
@@ -1,11 +1,13 @@
 package edu.vanderbilt.vandysports;
 
+import java.util.*;
+
 public class Team
 {
 	public final String Sport;
 	public final String Website_Link;
 	public final String Gender;
-	
+	public Vector<Event> events;
 	
 	public Team(String sport, String link, String gender)
 	{
