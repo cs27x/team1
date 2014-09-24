bfb3a95107628f2e832ae42f4c762c8d3291ed17
Nathan Walker <nathan.b.walker@vanderbilt.edu>
2014-09-24 18:29:19 -0500

    add basic skeleton for unit tests.

---
+++ Asgn1/assigned_project/VandySportsApp/VandySports2.0/src/test/java/edu/vanderbilt/vandysports/testsuite/TestDataModel.java
@@ -0,0 +1,19 @@
+package edu.vanderbilt.vandysports.testsuite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDataModel {
+
+    @Test
+    public void testGetAllEvents() {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void testGetAllTeams() {
+        fail("Test not implemented");
+    }
+
+}
